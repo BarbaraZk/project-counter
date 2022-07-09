b0c1234339e4f34fedcffc6e93156f1471e497bb
BarbaraZk <zelek.barbara@gmail.com>
2022-07-09 12:20:36 +0200

    js changes

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 const Item =(props)=>{
-    
     const calcliters = (props.liters/props.km*100).toFixed(2)
-    const calcall= (calcliters*props.km/100*props.price).toFixed(2)
-    
+    const calcall= (calcliters*props.km/100*props.price).toFixed(2) 
     const calc100km = (calcall/props.km*100).toFixed(2)
     // const value= (props.liters&&props.km&&props.price)
     
     return(
         <header>
             <div> 
-              {props.liters<=0 ? "" : `Fuel consuption :  ${calcliters} l/km`}
+              {props.liters<=0 ? "" : `Fuel consumption :  ${calcliters} l/km`}
               {/* {props.liters<=0 ? "" : (props.liters/props.km*100).toFixed(2)} */}
             {/* {value<=0 ? "" : calcliters} 
             {value<=0 ? "" : " l/100km"} */}
             </div>
+
             <div>
+                {props.price<=0 ? "" : `The cost of the entire trip : ${calcall} `}
             {/* {value<=0 ? "" : "Koszt calej podrozy:"}
             {value<=0 ? "" : calcall} 
             {value<=0 ? "" : "zl"} */}
@@ -44,13 +44,10 @@ class App extends React.Component {
 
      handleChange=(e)=>{
          this.setState({
-            [e.target.name]: e.target.value,
-            
+            [e.target.name]: e.target.value, 
          })
-
      }
 
-     
      handleSubmit=(e)=>{
         e.preventDefault()
         this.setState({
@@ -58,7 +55,6 @@ class App extends React.Component {
         })
       }
      
-  
      displayMessage=()=>{
          if(this.state.isSubmited){
              return <Item
@@ -80,20 +76,18 @@ class App extends React.Component {
     render() { 
         const {liters, km, price, isSubmited} = this.state;
         return <div class="App">
-            <h1>Check your fuel consuption </h1>
+            <h1>Calculate Your Fuel Consumption </h1>
             <br />
-            
-            
                 <form onSubmit={this.handleSubmit} class="formular">
-                    <label>Fuel burned in liters:
+                    <label>The number of litres of fuel you put in:
                         <input name="liters" value={this.state.liters} onChange={this.handleChange} type="number"/>
                     </label>
                     <br/>
-                    <label>Number of km:
+                    <label>The number of km since the last fill:
                         <input name="km" value={this.state.km} onChange={this.handleChange} type="number"/>
                     </label>
                     <br/>
-                    <label>Price of fuel :
+                    <label>The cost per litre of fuel:
                         <input name="price" value={this.state.price} onChange={this.handleChange} type="number"/>
                     </label>
                     <br/>
