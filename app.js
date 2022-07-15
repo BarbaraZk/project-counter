01e86b56103b92ab6d923914cf4c54d1cd6f95f1
BarbaraZk <zelek.barbara@gmail.com>
2022-07-15 17:05:41 +0200

    style update

--- app.js
+++ app.js
@@ -5,21 +5,21 @@ const Item =(props)=>{
     // const value= (props.liters&&props.km&&props.price)
     
     return(
-        <header>
-            <div> 
-              {props.liters<=0 ? "" : `Fuel consumption :  ${calcliters} l/km`}
+        <header className="display m-2 mt-4">
+            <h5 > 
+              {props.liters<=0 ? "" : `Fuel consumption : ${calcliters}l/km`}
               {/* {props.liters<=0 ? "" : (props.liters/props.km*100).toFixed(2)} */}
             {/* {value<=0 ? "" : calcliters} 
             {value<=0 ? "" : " l/100km"} */}
-            </div>
+            </h5>
 
-            <div>
+            <h5>
                 {props.price<=0 ? "" : `The cost of the entire trip : ${calcall} `}
             {/* {value<=0 ? "" : "Koszt calej podrozy:"}
             {value<=0 ? "" : calcall} 
             {value<=0 ? "" : "zl"} */}
             
-            </div>
+            </h5>
             <div>
             {/* {value<=0 ? "" : "Koszt 100 km:"}
             {value<=0 ? "" : calc100km} 
@@ -75,38 +75,40 @@ class App extends React.Component {
     
     render() { 
         const {liters, km, price, isSubmited} = this.state;
-        return <div class="container">
-            <h1>Calculate Your Fuel Consumption </h1>
-            <br />
-                <form onSubmit={this.handleSubmit} class="formular">
-                    <label>The number of litres of fuel you put in:
-                        <input name="liters" value={this.state.liters} onChange={this.handleChange} type="number"/>
-                    </label>
-                    <br/>
-                    <label>The number of km since the last fill:
-                        <input name="km" value={this.state.km} onChange={this.handleChange} type="number"/>
-                    </label>
-                    <br/>
-                    <label>The cost per litre of fuel:
-                        <input name="price" value={this.state.price} onChange={this.handleChange} type="number"/>
-                    </label>
-                  <br />
-                    <button onClick={this.handleReset} type="button" class="btn btn-outline-dark btn-sm" >RESET</button>
+        return <div className="container">
+                <br />
+                <div className="row d-flex justify-content-center align-items-center">
+                    <div className="col-5 md={6} ">
+                        <div className="form-group">
+                        <h3 className="display-6 titel">Check Your Fuel Consumption </h3>
+                            <form onSubmit={this.handleSubmit}>
+                                <label >The number of litres of fuel you put in:
+                                <input name="liters" className="form-control w-50 m-2" value={this.state.liters} onChange={this.handleChange} type="number"/>
+                                </label>
+                                <br/>
+                                <label>The number of km since the last fill:
+                                <input name="km" className="form-control w-50 m-2" value={this.state.km} onChange={this.handleChange} type="number"/>
+                                </label>
+                                <br/>
+                                <label>The cost per litre of fuel:
+                                <input name="price" className="form-control w-50 m-2" value={this.state.price} onChange={this.handleChange} type="number"/>
+                                </label>
+                                <br />
+                                <button onClick={this.handleSubmit} type="button" className="btn btn-success btn-sm m-2" >COUNT</button>
+                                <button onClick={this.handleReset} type="button" className="btn btn-outline-success btn-sm" >RESET</button>
                  
-                    <button type="button" class="btn btn-outline-dark btn-sm" >COUNT</button>
                     {/* <Item liters={liters} km={km} price={price} /> */}
                     {this.displayMessage()}
                     
-                </form>
-               
-                
-              
+                            </form>
+                        </div>
+                     </div>
             
+                    <div className="col"><img src="/images/1.gif" className="w-100"></img></div>
             </div>
-            
-            
-
-         
+           
+            </div>
+    
     }
 }
 
